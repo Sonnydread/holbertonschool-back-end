ad7861df52fa7a1f04e72d03891ffe02bf06f3d8
Sonnydread <alvaradoguille590@gmail.com>
2023-08-25 08:11:52 -0700

    Jsonmode

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
-"""
-Check student JSON output
-"""
-
-import json
+"""extend the script to export data in the JSON format"""
 import requests
-import sys
-
-users_url = "https://jsonplaceholder.typicode.com/users?id="
-todos_url = "https://jsonplaceholder.typicode.com/todos"
-
-
-def user_info(id):
-    """ Check user info """
-
-    with open(str(id) + '.json', 'r') as f:
-        student_json = json.load(f)
-
-    if student_json.get(str(id)) and len(student_json) == 1:
-        print("Correct USER_ID: OK")
-    else:
-        print("Correct USER_ID: Incorrect")
+from requests import get
+from sys import argv
+from json import dump
 
 
-if __name__ == "__main__":
-    user_info(int(sys.argv[1]))
+if __name__ == '__main__':
+    source = "https://jsonplaceholder.typicode.com/user/{}/todos".format(
+        argv[1])
+    names = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+    todores = get(source).json()
+    nameres = get(names).json()
+
+    listall = []
+    for todo in todores:
+        todo_direct = {}
+        todo_direct.update({"task": todo.get("title"), "completed": todo.get(
+            "completed"), "username": nameres.get("username")})
+        listall.append(todo_direct)
+
+    with open("{}.json".format(argv[1]), 'w') as f:
+        dump({argv[1]: listall}, f)
