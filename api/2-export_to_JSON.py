174affb2d3cf84c445b4e2935a02460225ca7981
Sonnydread <alvaradoguille590@gmail.com>
2023-08-25 07:48:43 -0700

    Jsonmode

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -2,25 +2,35 @@
 """extend the script to export data in the JSON format"""
 
 import json
-from json import dump
 import requests
-from requests import get
 from sys import argv
 
 
 if __name__ == '__main__':
     source = f"https://jsonplaceholder.typicode.com/user/{argv[1]}"
-    names = f"https://jsonplaceholder.typicode.com/users/{argv[1]}/todos"
-    uss = requests.get(source)
-    alltod = requests.get(names)
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{argv[1]}/todos"
+    user_response = requests.get(source)
+    todos_response = requests.get(todos_url)
+
+    if user_response.status_code != 200 or todos_response.status_code != 200:
+        print("Error: Unable to fetch data from the API.")
+        exit(1)
+
+    user_data = user_response.json()
+    todos_data = todos_response.json()
+
     listall = []
 
-    for z in todo.json():
-        todo_direct = {'task': f"{z.get('title')}",
-                       'completed': z.get('completed'),
-                       'username': f"{user.json().get('username')}"}
+    for todo in todos_data:
+        todo_direct = {
+            'task': todo.get('title'),
+            'completed': todo.get('completed'),
+            'username': user_data.get('username')
+        }
         listall.append(todo_direc)
 
     zm = {f"{argv[1]}": listall}
     with open(f"{argv[1]}.json", 'w') as file:
-        json.dump(zm, file)
+        json.dump(zm, file, indent=4)
+
+    print(f"Data saved to {argv[1]}.json")
